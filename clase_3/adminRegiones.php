c8bfd93af54209ba8348e1e1d92c0cdc1437f067
Matias Noriega <matiasnoriega86@gmail.com>
2018-05-03 20:26:09 -0300

    se instancia Region y se trae listado de regiones

--- clase_3/adminRegiones.php
+++ clase_3/adminRegiones.php
@@ -1,46 +1,57 @@
 <?php
-    include 'templates/header.php';
+include('templates/header.php');
+require('clases/Conexion.php');
+require('clases/Region.php');
+
+$objRegion = new Region();
+$listado = $objRegion->listarRegiones();
 ?>
 
-    <section class="container">
-        <h1>Panel de Administración de Regiones</h1>
+<section class="container">
+    <h1>Panel de Administración de Regiones</h1>
 
-        <table class="table table-striped table-hover">
-            <thead class="thead-dark">
+    <table class="table table-striped table-hover">
+        <thead class="thead-dark">
             <tr>
                 <th scope="col" width="10%">#</th>
                 <th scope="col" width="65%">Nombre región</th>
                 <th scope="col" colspan="2">
                     <a class="btn btn-success" href="formAgregarRegion.php">
-                    <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                     agregar
+                        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                        agregar
                     </a>
                 </th>
             </tr>
-            </thead>
+        </thead>
 
-            <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td> Nombre </td>
-                <td>
-                    <a class="btn btn-warning" href="formEditarRegion.php?regID=1">
-                    <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                    modificar
-                    </a>
-                </td>
-                <td>
-                    <a class="btn btn-danger" href="formEliminarRegion.php?regID=1">
-                    <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
-                    eliminar
-                    </a>
-                </td>
-            </tr>
-            </tbody>
-        </table>
+        <tbody>
+            <?php
+            foreach($listado as $region){
+            ?>
+                <tr>
+                    <th scope="row"><?php echo $region['regID']; ?></th>
+                    <td> <?php echo $region['regNombre']; ?> </td>
+                    <td>
+                        <a class="btn btn-warning" href="formEditarRegion.php?regID=<?php echo $region['regID']; ?>">
+                            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                            modificar   
+                        </a>
+                    </td>
+                    <td>
+                        <a class="btn btn-danger" href="formEliminarRegion.php?regID=<?php echo $region['regID']; ?>">
+                            <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                            eliminar
+                        </a>
+                    </td>
+                </tr>
+            <?php
+            }
+            ?>
+        </tbody>
+    </table>
 
-    </section>
+</section>
 
 <?php
-    include 'templates/footer.php';
+include 'templates/footer.php';
 ?>
