f4cee051fd150ff272b794bb90bc00849980976c
Matias Noriega <matiasnoriega86@gmail.com>
2018-05-19 09:31:13 -0300

    fix menor

--- clase_3/formAgregarDestino.php
+++ clase_3/formAgregarDestino.php
@@ -9,64 +9,68 @@ $listadoRegiones = $objRegion->listarRegiones();
 
 ?>
 
-<section class="container">
-     <?php
-    if(isset($_POST['submit'])){
+    <section class="container">
+        <?php
+    if(isset($_POST['submit'])){ ?>
 
-       
-        <div class="alert alert-<?= ($resultado) ? 'success' : 'danger'?>" role="alert">
-          <?= ($resultado) ? 'Agregado con &Eacute;xito' : 'Hubo un Error'?>
-      </div>
-      <a href="adminDestinos.php" class="btn btn-default">Volver al Listado de Destinos</a>
 
-      <?php } ?>
-      <h1>Formulario de alta de una nueva Destino</h1>
-
-      <div class="well">
-        <form action="formAgregarDestino.php" method="post">
-            <div class="form-group form-group-lg">
-                <input type="text" name="destNombre" class="form-control" placeholder="Nombre del Destino" required>
+            <div class="alert alert-<?= ($resultado) ? 'success' : 'danger'?>" role="alert">
+                <?= ($resultado) ? 'Agregado con &Eacute;xito' : 'Hubo un Error'?>
             </div>
+            <?php } ?>
+            <a href="adminDestinos.php" class="btn btn-default">Volver al Listado de Destinos</a>
 
-            <div class="form-group form-group-lg">
-                <select class="form-control" name="regID" id="regID" required>
-                    <option value="" disabled selected>Elegir una Regi&oacute;n</option>
-                    <?php
+
+            <h1>Formulario de alta de una nueva Destino</h1>
+
+            <div class="well">
+                <form action="formAgregarDestino.php" method="post">
+                    <div class="form-group form-group-lg">
+                        <input type="text" name="destNombre" class="form-control" placeholder="Nombre del Destino" required>
+                    </div>
+
+                    <div class="form-group form-group-lg">
+                        <select class="form-control" name="regID" id="regID" required>
+                            <option value="" disabled selected>Elegir una Regi&oacute;n</option>
+                            <?php
                     foreach($listadoRegiones as $region){
                         ?>
 
-                        <option value="<?php echo $region['regID'] ?>"><?php echo $region['regNombre'] ?></option>
+                                <option value="<?php echo $region['regID'] ?>">
+                                    <?php echo $region['regNombre'] ?>
+                                </option>
 
-                        <?php } ?>
-                    </select>
-                </div>
-                <div class="form-group form-group-lg">
-                    <div class="input-group input-group-lg">
-                      <span class="input-group-addon">$</span>
-                      <input type="number" name="destPrecio" class="form-control" placeholder="Ingresar Precio" required>
-                  </div>
-              </div>
-              <div class="form-group form-group-lg">
-                <div class="input-group input-group-lg">
-                    <span class="input-group-addon">#</span>
-                    <input type="number" name="destAsientos" class="form-control" placeholder="Ingresar Cantidad de Asientos Totales" required>
-                </div>
+                                <?php } ?>
+                        </select>
+                    </div>
+                    <div class="form-group form-group-lg">
+                        <div class="input-group input-group-lg">
+                            <span class="input-group-addon">$</span>
+                            <input type="number" name="destPrecio" class="form-control" placeholder="Ingresar Precio" required>
+                        </div>
+                    </div>
+                    <div class="form-group form-group-lg">
+                        <div class="input-group input-group-lg">
+                            <span class="input-group-addon">#</span>
+                            <input type="number" name="destAsientos" class="form-control" placeholder="Ingresar Cantidad de Asientos Totales" required>
+                        </div>
+                    </div>
+                    <div class="form-group form-group-lg">
+                        <div class="input-group input-group-lg">
+                            <span class="input-group-addon">#</span>
+                            <input type="number" name="destDisponibles" class="form-control" placeholder="Ingresar Cantidad de Asientos Disponibles"
+                                required>
+                        </div>
+                    </div>
+                    <div class="form-group form-group-lg">
+                        <input type="submit" name="submit" value="Agregar Destino" class="btn btn-success">
+                        <a href="adminDestinos.php" class="btn btn-default">Volver al Listado de Destinos</a>
+                    </div>
+                </form>
             </div>
-            <div class="form-group form-group-lg">
-                <div class="input-group input-group-lg">
-                    <span class="input-group-addon">#</span>
-                    <input type="number" name="destDisponibles" class="form-control" placeholder="Ingresar Cantidad de Asientos Disponibles" required>
-                </div>
-            </div>
-            <div class="form-group form-group-lg">
-                <input type="submit" name="submit" value="Agregar Destino" class="btn btn-success">
-                <a href="adminDestinos.php" class="btn btn-default">Volver al Listado de Destinos</a>
-            </div>
-        </form>
-    </div>
 
-</section>
+    </section>
 
-<?php
+    <?php
 include 'templates/footer.php';
 ?>
